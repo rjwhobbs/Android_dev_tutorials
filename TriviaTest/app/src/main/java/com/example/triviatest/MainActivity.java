4b56bbe5963322dac1a04952d00914cdc99e0a39
Roger Hobbs - BCX <rhobbs@student.wethinkcode.co.za>
2020-02-24 09:28:13 +0200

    Why don't we update inside the callback

--- TriviaTest/app/src/main/java/com/example/triviatest/MainActivity.java
+++ TriviaTest/app/src/main/java/com/example/triviatest/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,7 +16,7 @@ import com.example.triviatest.model.Question;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView questionTextview;
     private TextView questionCounterTextview;
@@ -24,18 +25,64 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton nextButton;
     private ImageButton prevButton;
     private int currentQuestionIndex = 0;
-
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Question> questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+        nextButton = findViewById(R.id.next_button);
+        prevButton = findViewById(R.id.prev_button);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        questionCounterTextview = findViewById(R.id.counter_text);
+        questionTextview = findViewById(R.id.question_textview);
+
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+
+        questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
+
+                questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
                 Log.d("xx1", "processFinished: " + questionArrayList);
             }
         });
+        Log.d("xx2", "processFinished: " + questionList);
+    }
+
+    public void debug() {
+        Log.d("xx3", "debug " + questionList);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.prev_button:
+
+                break;
+            case R.id.next_button:
+                currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+                updateQuestion();
+                break;
+
+            case R.id.true_button:
+                break;
+
+            case R.id.false_button:
+                break;
+
+        }
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        questionTextview.setText(question);
+//        questionCounterTextview.setText(currentQuestionIndex + " / " + questionList.size()); // 0 / 234
+
     }
 }
